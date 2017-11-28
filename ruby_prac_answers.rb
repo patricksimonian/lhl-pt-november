711b0ebb9a619ed694c92b9c8b31552f1ae26b37
Mingwei <mingwei.y@gmail.com>
2017-11-27 17:22:24 -0800

    Update ruby_prac_answers.rb
    Wayne's answers

--- ruby_prac_answers.rb
+++ ruby_prac_answers.rb
@@ -32,6 +32,52 @@
 #=====================================================================================================================
 ## Wayne
 
+# Q1 Answer
+def getProduct(n, y)
+  (n * y).to_s
+end
+# END of Q1 Answer
+
+# Q2 Answer
+def find_if_even(n)
+  n % 2 == 0
+end
+# END of Q2 Answer
+
+# Q3 Answer
+def even_or_odd_int(n)
+  "this number is #{find_if_even(n) ? 'even' : 'odd'}"
+end
+# END of Q3 Answer
+
+# Q4 Answer
+def even_or_odd_number(n)
+  even_or_odd_int(n.round) 
+end
+# END of Q4 Answer
+
+# Q5 Answer
+def even_or_odd_product(x, y)
+  product = getProduct(x, y).to_i
+  %{Calculating #{x} times #{y}
+Result #{product}
+The result of this number happens to be #{find_if_even(product) ? 'even' : 'odd'}
+}
+end
+# END of Q5 Answer
+
+# Q6 Answer
+def findFirstIndex(character, test_string)
+  index = 0
+  test_string.each_char do |c|
+    break if c == character
+    index += 1
+  end 
+
+  index == test_string.length ? "no match found" : index
+end
+# END of Q6 Answer
+
 ## End of Wayne's Answers'
 #=====================================================================================================================
 ## Zac
